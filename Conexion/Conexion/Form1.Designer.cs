ba6c6e27b2702067ddc80aef69667580c22691f5
Alexis Rauda <126432796+RBryan05@users.noreply.github.com>
2024-08-06 15:04:03 -0600

    Aplicación de comando SELECT

---
+++ Conexion/Conexion/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Conexion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCustomerID = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(155, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese un ID";
+            // 
+            // lblCustomerID
+            // 
+            this.lblCustomerID.Location = new System.Drawing.Point(287, 131);
+            this.lblCustomerID.Name = "lblCustomerID";
+            this.lblCustomerID.Size = new System.Drawing.Size(100, 26);
+            this.lblCustomerID.TabIndex = 1;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(425, 121);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(124, 46);
+            this.btnEnviar.TabIndex = 2;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.lblCustomerID);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox lblCustomerID;
+        private System.Windows.Forms.Button btnEnviar;
+    }
+}
+
---
+++ Conexion/Conexion/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conexion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            var conexion = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            SqlConnection com = new SqlConnection(conexion);
+
+            com.Open();
+
+            String sql = "";
+            sql = sql + "SELECT [CustomerID] " + "\n";
+            sql = sql + "      ,[CompanyName] " + "\n";
+            sql = sql + "      ,[ContactName] " + "\n";
+            sql = sql + "      ,[ContactTitle] " + "\n";
+            sql = sql + "      ,[Address] " + "\n";
+            sql = sql + "      ,[City] " + "\n";
+            sql = sql + "      ,[Region] " + "\n";
+            sql = sql + "      ,[PostalCode] " + "\n";
+            sql = sql + "      ,[Country] " + "\n";
+            sql = sql + "      ,[Phone] " + "\n";
+            sql = sql + "      ,[Fax] " + "\n";
+            sql = sql + "  FROM [dbo].[Customers]";
+
+            SqlCommand comandos = new SqlCommand(sql);
+            var lectura = comandos.BeginExecuteReader();
+        }
+    }
+}
